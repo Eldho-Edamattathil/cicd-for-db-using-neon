06123bb2e26aad60a9953e111c7edd7ef5212b8a
Eldho George <142223023+Eldho-Edamattathil@users.noreply.github.com>
2025-04-06 17:01:15 +0530

    Update 1698765432345_create-todos-table.js

--- migrations/1698765432345_create-todos-table.js
+++ migrations/1698765432345_create-todos-table.js
@@ -2,8 +2,11 @@ exports.up = (pgm) => {
   pgm.createTable('todos', {
     id: 'id',
     title: { type: 'varchar(255)', notNull: true },
-    name: { type: 'varchar(255)', notNull: false },
     completed: { type: 'boolean', notNull: true, default: false },
+	  created_at: {
+		type: 'timestamp',
+		notNull: true,
+		default: pgm.func('current_timestamp'),},
 	 
 	  
   });
